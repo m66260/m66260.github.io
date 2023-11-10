6d0258bf90a8ae2b9336f184c2595e471ed7ab63
m66260 <m66260@protonmail.com>
2023-11-10 15:23:54 -0500

    test safe app

--- client/setupProxy.ts
+++ client/setupProxy.ts
@@ -1,11 +1,14 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
+// import { createProxyMiddleware } from "http-proxy-middleware";
 
 module.exports = function (app) {
-  app.use(
-    "/**",
-    createProxyMiddleware({
-      target: "https://api.binance.com",
-      changeOrigin: true,
-    })
-  );
+  app.use("/manifest.json", function (req, res, next) {
+    res.set({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    });
+
+    next();
+  });
 };
